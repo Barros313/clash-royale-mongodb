1084a92c74edbdecdacd92614a8ae12aa3d6c9ad
Barros313 <gbarros1337@gmail.com>
2024-10-05 22:42:30 -0300

    Separated configurations in a different file
    Signed-off-by: Barros313 <gbarros1337@gmail.com>

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+from typing import override
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+# Load environment variables
+load_dotenv()
+
+# Get Clash Royale API Key
+CR_API_KEY: str = os.getenv("CLASH_ROYALE_API")
+# Get MongoDB URI String connection
+MONGO_URI: str = os.getenv("MONGO_ATLAS_URI")
+# Define API Base URL
+BASE_URL: str = 'https://api.clashroyale.com/v1'
+
+# Header with API Key
+headers = {
+    'Authorization': f'Bearer {CR_API_KEY}'
+}
+
+# MongoClient instance
+client: MongoClient = MongoClient(f'{MONGO_URI}')
+# Database selection
+db = client['clash_royale']
+
+# Hashtag URL encoded
+HASH_URL_ENCODE: str = '%23'
--- leaderboard.py
+++ leaderboard.py
@@ -2,27 +2,9 @@ from typing import Any, Optional, Dict, List
 from urllib import request, response
 import requests
 import pymongo
-from pymongo import MongoClient
-import os
-from dotenv import load_dotenv
+from config import headers, db, BASE_URL, HASH_URL_ENCODE
 
-load_dotenv()
-
-CLASH_ROYALE_API_KEY: str = os.getenv("CLASH_ROYALE_API")
-MONGO_URL: str = os.getenv("MONGO_URL")
-
-API_KEY: str = f'{CLASH_ROYALE_API_KEY}'
-BASE_URL: str = 'https://api.clashroyale.com/v1'
-
-HASH_URL_ENCODE: str = '%23'
-
-client: MongoClient = MongoClient(f'{MONGO_URL}')
-db = client['clash_royale']
 collection = db['players']
-
-headers: Dict[str, str] = {
-    'Authorization': f'Bearer {API_KEY}'
-}
 
 def main() -> None:
     leaderboard_id : Optional[int] = fetch_leaderboard()
@@ -46,10 +28,16 @@ def main() -> None:
 
 def insert_players(top_players):
     for player in top_players:
-        collection.insert_one(get_player_info(player['tag'][1:]))
+        # Removing Hash from tag string
+        tag_without_hash = player['tag'][1:]
+        
+        # Fetch player info
+        player_info = get_player_info(tag_without_hash)
+        # Add to collection
+        collection.insert_one(player_info)
 
 
-def get_player_info(player_tag):
+def get_player_info(player_tag: str):
     url = f'{BASE_URL}/players/{HASH_URL_ENCODE}{player_tag}'
     res = requests.get(url, headers=headers)
 
@@ -61,11 +49,11 @@ def get_player_info(player_tag):
         return None
 
 
-def filter_top_players(players: List[Dict[str, Any]], limit: int = 40) -> List[Dict[str, Any]]:
+def filter_top_players(players, limit: int = 40):
     return players[:min(len(players), limit)]
 
 
-def get_ranking(leaderboard_id : int) -> str | None:
+def get_ranking(leaderboard_id: int) -> str | None:
     url : str = f'{BASE_URL}/leaderboard/{leaderboard_id}'
     res : response = requests.get(url, headers=headers)
 
@@ -91,7 +79,7 @@ def fetch_leaderboard() -> Optional[int]:
 
 
 
-def get_leaderboard_id(res : Dict[str, Any]) -> Optional[int]:
+def get_leaderboard_id(res: Dict[str, Any]) -> Optional[int]:
     try:
         return int(res['items'][0]['id'])
     except (KeyError, ValueError, TypeError) as exp:
